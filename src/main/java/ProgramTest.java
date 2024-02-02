02f0f88480ecb53bc7bb65d1f9adb8dea57ed601
Andrea Fraschini <andrea1.fraschini@mail.polimi.it>
2024-02-02 22:26:32 +0100

    checazzo

--- src/main/java/ProgramTest.java
+++ src/main/java/ProgramTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ProgramTest {
-    private final Program program = new Program();
+    private static final Program program = new Program();
 
     @Test
     void add() {
