3ee81d70f91b261f1abc02cb8659aa5c2541bf3e
Andrea Fraschini <andrea1.fraschini@mail.polimi.it>
2024-02-02 22:12:57 +0100

    spetta un attimo

--- src/main/java/ProgramTest.java
+++ src/main/java/ProgramTest.java
@@ -3,11 +3,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ProgramTest {
-    private static final Program program = new Program();
+    private final Program program = new Program();
 
     @Test
     void add() {
-        assertEquals(-6, program.add(2, -8));
+        assertEquals(6, program.add(2, -8));
     }
 
     @Test
