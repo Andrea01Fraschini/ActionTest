071890c012e61b1251d1132a46e535a42f2b5ef1
Andrea Fraschini <andrea1.fraschini@mail.polimi.it>
2024-02-02 16:02:19 +0100

    ci si prova 34

--- src/main/java/ProgramTest.java
+++ src/main/java/ProgramTest.java
@@ -3,9 +3,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ProgramTest {
+    private static final Program program = new Program();
     @Test
     void add() {
-        assertEquals(-6, Program.add(2, -8));
+        assertEquals(-6, program.add(2, -8));
     }
 
     @Test
