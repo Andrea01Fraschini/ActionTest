5c94f41b741693fb859ed1d288dd8159e715cb1d
Andrea Fraschini <andrea1.fraschini@mail.polimi.it>
2024-02-02 15:15:02 +0100

    ci si prova

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,3 +1,7 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Program {
     public static void main(String[] args) {
         System.out.println("Hello world!");
@@ -5,5 +9,15 @@ public class Program {
 
     public static int add(int a, int b){
         return a+b;
+    }
+
+    @Test
+    void add() {
+        assertEquals(-6, add(2, -8));
+    }
+
+    @Test
+    void shouldPass() {
+        assertEquals(-6, -6);
     }
 }
