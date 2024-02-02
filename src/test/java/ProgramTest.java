9c3f29bbc3819a05c1c291d26ffa29f570b5b288
Andrea Fraschini <andrea1.fraschini@mail.polimi.it>
2024-02-02 01:16:01 +0100

    fixed

--- src/test/java/ProgramTest.java
+++ src/test/java/ProgramTest.java
@@ -1,11 +1,11 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ProgramTest {
-
     @Test
     void add() {
         assertEquals(-6, Program.add(2, -8));
     }
+
 }
