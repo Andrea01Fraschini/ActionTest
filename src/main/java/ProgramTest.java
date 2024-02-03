435383de8474694b97fbbd59b214f906abcbd475
Andrea01Fraschini <andrea1.fraschini@mail.polimi.it>
2024-02-03 18:57:59 +0100

    Update ProgramTest.java

--- src/main/java/ProgramTest.java
+++ src/main/java/ProgramTest.java
@@ -6,18 +6,18 @@ class ProgramTest {
     private static final Program program = new Program();
 
     @Test
-    void add() {
+    void add_SHOULD_FAIL() {
         try{
             throw new RuntimeException("MESSAGGIO");
         }catch (Exception e){
 
         }
 
-        assertEquals(-6, program.add(2, -8));
+        assertEquals(6, program.add(2, -8));
     }
 
     @Test
-    void add2() {
+    void add2_SHOULD_PASS() {
         assertEquals(489, program.add(69, 420));
     }
 
